a0b5eaf0cef210fab309ab1bb7ba30550fc94fef
engin <engin.c89@gmail.com>
2014-11-06 23:14:57 +0200

    a

--- Population.py
+++ Population.py
@@ -4,14 +4,10 @@ Created on Nov 4, 2014
 @author: engin
 '''
 
-import pandas as pd
-
-def readData(file_Name):
-    data = pd.read_excel(file_Name, 'fixhtjNY')
-    print data.dropna().head(80)
-    pass 
-
+from fileUtil import *
 
 if __name__ == "__main__":
-    readData('1965.xlsx')
-    
+    fs = find_file_names('years', '*.xls')
+    for f in fs:
+        data = get_names_data(f, 'xls')
+        print data
--- fileUtil.py
+++ fileUtil.py
@@ -3,21 +3,23 @@ Created on Nov 4, 2014
 
 @author: engin
 '''
-
+# -*- coding: utf-8 -*-
 import glob, os
 import pandas as pd
 
-def get_names_data():
-    frame_array = []
-    files = glob.glob(os.path.join('years','*.xlsx'))
-    for file in files:
-        xl = pd.ExcelFile(file)
-        sheetNames = xl.sheet_names
-        for sheetname in sheetNames:
-            data = pd.read_excel(file, sheetname)
-            print data.head(15)
-    
-    names = pd.concat(frame_array, ignore_index=True)
-    return names
-
-get_names_data()
+def get_names_data(fileName, file_type):
+    fName = fileName[6:10]
+    print fName, " is now reading"
+    if file_type == 'csv':
+        with open(fileName, 'r') as f:
+            data = pd.read_csv(f, error_bad_lines=False, delimiter=';')
+    elif file_type == 'xlsx' or 'xls':
+        fileX = pd.ExcelFile(fileName)
+        sheetNames = fileX.sheet_names
+        for sheet in sheetNames:
+            data = pd.read_excel(fileName, sheet)
+    return data.dropna()
+        
+def find_file_names(path, fileName_extension):
+    files = glob.glob(os.path.join(path, fileName_extension))
+    return files
